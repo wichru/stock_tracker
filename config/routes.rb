08601e9e56b22264d72115a419457fd0fb8668cd
wichru <wicher.maciej@gmail.com>
2019-01-08 20:12:11 +0100

    Add create action for UserStocks

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'user_stocks/create'
   devise_for :users
   root 'welcome#index'
   get 'my_portfolio', to: 'users#my_portfolio'
   get 'search', to: 'stocks#search'
+  resources :user_stocks, only: [:create]
 end
