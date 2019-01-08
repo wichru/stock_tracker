3cc814837a0b43bee7b09003c157b2746d55fc4f
wichru <wicher.maciej@gmail.com>
2019-01-08 17:25:21 +0100

    Refactor code and handling with flash errors

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,19 +1,14 @@
 class StocksController < ApplicationController
 
   def search
-    if params[:stock].present?
-      @stock = Stock.new_from_lookup(params[:stock])
-      if @stock
-        respond_to do |format|
-          format.js { render partial: 'users/result' }
-        end
-      else
-        flash[:danger] = "Ticker doesn't exist"
-        redirect_to my_portfolio_path
-      end
+    if params[:stock].blank?
+      flash.now[:danger] = 'You entered an empty ticker'
     else
-      flash[:danger] = 'You entered an empty ticker'
-      redirect_to my_portfolio_path
+      @stock = Stock.new_from_lookup(params[:stock])
+      flash.now[:danger] = "Ticker doesn't exist" unless @stock
+    end
+    respond_to do |format|
+      format.js { render partial: 'users/result' }
     end
   end
 end
