aadce777a37cb4ed1261f1d2fd79fd6cac49ff8c
wichru <wicher.maciej@gmail.com>
2019-01-08 22:09:30 +0100

    Add restrictions to stock model

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -3,6 +3,10 @@ class Stock < ApplicationRecord
   has_many :user
   has_many :user_stocks
 
+  def self.find_by_ticker(ticker_symbol)
+    where(ticker: ticker_symbol).first
+  end
+
   def self.new_from_lookup(ticker_symbol)
     looked_up_stock = StockQuote::Stock.quote(ticker_symbol)
     new(name: looked_up_stock.company_name, ticker: looked_up_stock.symbol,
