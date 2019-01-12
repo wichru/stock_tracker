39b186dce35ec12368d61bce037d965125b09ff0
wichru <wicher.maciej@gmail.com>
2019-01-12 23:39:46 +0100

    Add missing stock spec

--- spec/models/stock_spec.rb
+++ spec/models/stock_spec.rb
@@ -3,4 +3,17 @@ require 'rails_helper'
 RSpec.describe Stock, type: :model do
   it { should have_many(:users).through(:user_stocks) }
   it { should have_many(:user_stocks) }
+
+  describe 'Searching stocks' do
+
+    it 'finds stock by ticker' do
+      Stock.create(ticker: 'GS')
+      expect(Stock.find_by_ticker('GS')).not_to eq nil
+    end
+
+    it 'returns stock details' do
+      stock = Stock.new_from_lookup('GS')
+      expect(stock).to_not eq nil
+    end
+  end
 end
